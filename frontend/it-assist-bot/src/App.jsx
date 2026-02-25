c7f73c2520c710a7687b2d19776957f2ca699e35
sheethana14 <sheethana1412@gmail.com>
2026-02-25 20:09:07 +0530

    add pdf upload page with backend integration

--- frontend/it-assist-bot/src/App.jsx
+++ frontend/it-assist-bot/src/App.jsx
@@ -1,5 +1,20 @@
-function APP(){
-  return <h1> Internal IT Assist Bot</h1>
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import UploadPage from "./pages/uploadPage";
+
+function App() {
+  return (
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <nav>
+          <Link to="/upload">Upload</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default APP;
+export default App;
