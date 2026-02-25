06507a3aea4eed52b8414e5c18b8f72c8b0e1d61
sheethana14 <sheethana1412@gmail.com>
2026-02-25 20:21:16 +0530

    fixed the error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from app.api.upload import router as upload_router
+from app.api.chat import router as chat_router
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title = " IT Assist Bot ")
 
-app.include_router(
-    upload_router,
-    prefix="/api",
-    tags=["Upload"]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["http://localhost:5173"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                )
+
+app.include_router(upload_router,prefix="/api",tags=["Upload"]
 )
+app.include_router(chat_router,prefix="/api", tags=["Chat"])
+
 
 @app.get("/")
 def read_root():
