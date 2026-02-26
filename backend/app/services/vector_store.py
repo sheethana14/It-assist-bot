78964bc2d296e12826008837ceb0397872a03bdc
sheethana14 <sheethana1412@gmail.com>
2026-02-26 12:38:35 +0530

    support multi-document FAISS Persistence

--- backend/app/services/vector_store.py
+++ backend/app/services/vector_store.py
@@ -1,31 +1,49 @@
+import os
 import faiss
 import numpy as np
 from typing import List
+import pickle
 
-EMBEDDING_DIMENTION = 384
+index_PATH = "vector_store/faisss.index"
+METADATA_PATH ="vector_store/metadata.pkl"
+
+EMBEDDING_DIMENTION = 1538
 
 index = faiss.IndexFlatL2(EMBEDDING_DIMENTION)
 
 stored_chunks = []
 
-def add_to_index(embedding:List[List[float]], chunks: List[str]):
-    """
-    Add embeddings and corrsponding chunks to faiss index.
-    """
-    vectors = np.array(embedding).astype("float32")
+if os.path.exists(index_PATH):
+    index =faiss.read_index(index_PATH)
+    with open(METADATA_PATH,"rb") as f:
+        metadata_store = pickle.load(f)
+
+else:
+    index = faiss.IndexFlatL2(EMBEDDING_DIMENTION)
+    metadata_store = []
+
+def add_to_index(embedding, chunks, filename):
+    global metadata_store
+
+    vectors = np.array(embedding).astype('float32')
     index.add(vectors)
-    stored_chunks.extend(chunks)
 
-def search_index(query_embedding: List[float], top_k: int=3):
-    """
-    Search FAISS index and return top_k relevant chunks.
-    """
-    query_vector = np.array([query_embedding]).astype("float32")
+    for chunk in chunks:
+        metadata_store.append({
+            "text": chunk,
+            "source": filename
+        })
 
-    distances, indices = index.search(query_vector, top_k)
+    faiss.write_index(index, index_PATH)
+    with open(METADATA_PATH, "wb") as f:
+        pickle.dump(metadata_store, f)
 
-    results =[]
-    for idx in indices[0]:
-        if idx < len(stored_chunks):
-            results.append(stored_chunks[idx])
-    return results
+def search_index(query_embedding, top_k=3):
+    query_vector = np.array(query_embedding).astype('float32')
+    distances, indices =index.search(query_vector,top_k)
+
+    results = []
+    for idex in indices[0]:
+        if idex < len(metadata_store):
+            results.append(metadata_store[idex]["text"])
+        return results
