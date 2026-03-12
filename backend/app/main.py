1ae316b383126281a7199307175ec388c933bfae
sheethana14 <sheethana1412@gmail.com>
2026-03-12 22:48:47 +0530

    Implemented CORS middleware for API communication

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from app.api.upload import router as upload_router
 from app.api.chat import router as chat_router
 
+
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title = " IT Assist Bot ")
 
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:5173"],
+                   allow_origins=["http://localhost:5173","http://localhost:3000", "*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],
