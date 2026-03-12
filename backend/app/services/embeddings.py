42d60bfaf8838fa87ac7dc79bde9f9d838dd5cde
sheethana14 <sheethana1412@gmail.com>
2026-03-12 22:50:08 +0530

    Fixed embedding

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,16 +1,13 @@
 from sentence_transformers import SentenceTransformer
 from typing import List
 
-model = SentenceTransformer ("all-MiniLM-L6-V2")
+model = SentenceTransformer("all-MiniLM-L6-V2")
 
 def generate_embeddings(text_chunks: List[str]) -> List[List[float]]:
     """
-   convert test chunks into vector embedding.
-   :param text_chunks: List of text chunks
+    Convert text chunks into vector embeddings.
+    :param text_chunks: List of text chunks
     :return: List of embedding vectors
-
     """
-    embeddings = model.encode(text_chunks, 
-    convert_to_numpy= True
-    )
+    embeddings = model.encode(text_chunks, convert_to_numpy=True)
     return embeddings.tolist()
